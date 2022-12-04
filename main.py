cb7cdcfa823bd167b7a816bd91c8f5075e572fcd
Alex Jacobs <alexander.jacobs@proton.me>
2022-12-04 22:15:54 +0200

    Generate initial requirements.txt

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
+import jinja2
+
 print("This is a test of python!")
+
+environment = jinja2.Environment()
+template = environment.from_string("Hello, {{ name }}!")
+test = template.render(name="World")
+print(test)
