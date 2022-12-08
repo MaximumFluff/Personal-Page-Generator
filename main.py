724ceec5087e72ee5e9dd0f73dc7a8181cb32dfe
Alex Jacobs <alexander.jacobs@proton.me>
2022-12-08 19:40:22 +0200

    Skeleton of templates created. Successfully generating files

--- main.py
+++ main.py
@@ -1,8 +1,26 @@
-import jinja2
+from jinja2 import Environment, FileSystemLoader
+import os
 
-print("This is a test of python!")
+print("Generating static site...")
 
-environment = jinja2.Environment()
-template = environment.from_string("Hello, {{ name }}!")
-test = template.render(name="World")
-print(test)
+configuration = {
+    "index": "index.j2",
+    "resume": "resume.j2",
+    "blog": "blog.j2",
+}
+
+if os.path.exists("output") is False:
+    print("output folder generated")
+    os.makedirs("output")
+
+environment = Environment(loader=FileSystemLoader("templates/"))
+
+for key, value in configuration.items():
+    print("key", key)
+    print("Value", value)
+    template = environment.get_template(value)
+    content = template.render()
+    with open(f"output/{key}.html", mode="w", encoding="utf-8") as file:
+        file.write(content)
+        print("File created!")
+print("Static site generated!")
